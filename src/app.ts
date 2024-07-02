c3de6775eae0935dca07979520f2c5e805eb0311
hasibferdous <hasibferdous.ict@gmail.com>
2024-07-02 14:43:49 +0600

    modular pattern implemented

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,21 @@
-const express = require('express')
-const app = express()
-const port = 3000
+import cors from 'cors';
+import express, { Application, Request, Response } from 'express';
+import { StudentRoutes } from './app/modules/student/student.route';
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const app: Application = express();
 
-console.log(process.cwd());
+//parsers
+app.use(express.json());
+app.use(cors());
+
+// application routes
+app.use('/api/v1/students', StudentRoutes);
+
+const getAController = (req: Request, res: Response) => {
+  const a = 10;
+  res.send(a);
+};
+
+app.get('/', getAController);
+
+export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,17 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
+import app from './app';
+import config from './app/config';
 
 async function main() {
-  await mongoose.connect(process.env.DATABASE_URL);
+  try {
+    await mongoose.connect(config.database_url as string);
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+    app.listen(config.port, () => {
+      console.log(`app is listening on port ${config.port}`);
+    });
+  } catch (err) {
+    console.log(err);
+  }
 }
-app.listen(process.env.PORT, () => {
-    console.log(`Example app listening on port ${process.env.PORT}`)
-  })
+
+main();
